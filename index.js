1426b5c4c4b5bde90432a1473479cc482d8bd1bd
EA - Lennon Ribeiro Matsumoto Pinheiro <lennon_matsumoto@educadventista.org>
2023-11-21 22:12:32 -0300

    atividade update - 14/11/2023

--- index.js
+++ index.js
@@ -24,6 +24,27 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 // rotas
+
+app.post("/edit/save", (request, response) => {
+    const { id, title, pageqty } = request.body
+    
+
+    const sql = `
+    UPDATE books
+    SET title = '${title}', pageqty = '${pageqty}'
+    WHERE id = ${id}
+    `
+
+
+    conn.query(sql, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        response.redirect("/")
+    })
+})
+
 app.post("/register/save", (request, response) =>{
 
     const { title, pageqty} = request.body
@@ -45,6 +66,26 @@ app.post("/register/save", (request, response) =>{
         }
 
         response.redirect("/")
+    })
+})
+
+app.get("/edit/:id", (request, response) => {
+    const id = request.params.id
+
+    const sql = `
+    SELECT * FROM books
+    WHERE id = ${id}
+    `
+
+    conn.query(sql, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        const book = data[0]
+
+
+        response.render('edit', { book })
     })
 })
 
