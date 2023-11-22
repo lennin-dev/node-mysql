e39521df006eeb9dbef1b1e02ebfcf49b98df5d2
EA - Lennon Ribeiro Matsumoto Pinheiro <lennon_matsumoto@educadventista.org>
2023-11-21 22:29:48 -0300

    deletando dados do banco e completando o CRUD

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { response } = require("express")
+const { response, request } = require("express")
 const express = require("express")
 const exphbs = require("express-handlebars")
 const mysql = require("mysql2")
@@ -24,6 +24,24 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 // rotas
+
+
+app.post("/delete", (request, response) => {
+    const { id } = request.body
+
+    const sql = `
+    DELETE FROM books
+    WHERE id = ${id}
+    `
+
+    conn.query(sql, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        response.redirect("/")
+    })
+})
 
 app.post("/edit/save", (request, response) => {
     const { id, title, pageqty } = request.body
